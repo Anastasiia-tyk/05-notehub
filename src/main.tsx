5f3d4bd73e80b6a323c7af9c28f8059a496d51ec
Anastasiia <anastasiia0matsiuk@gmail.com>
2026-06-08 22:31:12 +0300

    init

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+// src/main.tsx
+
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./components/App/App";
+
+import "modern-normalize";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <QueryClientProvider client={queryClient}>
+        <App />
+    </QueryClientProvider>
+);
