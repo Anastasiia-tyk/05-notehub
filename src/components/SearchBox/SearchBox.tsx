f7fc919e6593ad28abe8e5efd92d72463cd612d7
Anastasiia <anastasiia0matsiuk@gmail.com>
2026-06-09 21:57:33 +0300

    update Search Box

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -2,12 +2,17 @@
 
 import css from "./SearchBox.module.css"
 
-export default function SearchBox() {
+interface SearchBoxProps {
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function SearchBox({ onChange }: SearchBoxProps) {
     return (
         <input
             className={css.input}
             type="text"
             placeholder="Search notes"
+            onChange={onChange}
         />
     );
 } 
